14e90949041c605e9bbff68a98f0341f5e927c84
ttinonin <tinonin935@gmail.com>
2022-06-15 17:23:46 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [post, setPost] = useState([])
+  const [pokemon, setPokemon] = useState(1)
+  const [sprite, setSprite] = useState("")
+  const [type, setType] = useState("")
+
+  useEffect(() => {
+    setPokemon(1)
+  }, [])
+
+  const baseURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  useEffect(() => {
+    axios
+      .get(baseURL)
+      .then((res) => {
+        setPost(res.data)
+        setSprite(res.data.sprites.front_default)
+        setType(res.data.types[0].type.name)
+      })
+  }, [pokemon])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h1>React Pokedex!</h1>
+      </div>
+      <div className='index__body'>
+        <h2>{post.name}</h2>
+        <img className='img' src={sprite}/>
+        <p>Type: {type}</p>
+        <input type="number" placeholder="Pokemon No." autoFocus autoComplete='off' className='query' onChange={(e) => setPokemon(e.target.value)}/>
+      </div>
     </div>
   );
 }
